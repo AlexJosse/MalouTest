6dbc466dcb509042995d068f6b31e8d7053be8b8
unknown <alexandre.josse@epitech.eu>
2021-06-16 20:24:02 +0200

    start front

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { GalleryComponent } from "./gallery/gallery.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, GalleryComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/src/app/gallery/gallery.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "../services/api.service";
+
+@Component({
+  selector: "app-gallery",
+  templateUrl: "./gallery.component.html",
+  styleUrls: ["./gallery.component.scss"]
+})
+export class GalleryComponent implements OnInit {
+  constructor(private apiService: ApiService) {}
+  Items: any;
+  lazyTargets: any;
+  ngOnInit() {
+    this.apiService.getImage().subscribe((result: any) => {
+      this.Items = result;
+      console.log(result);
+    });
+
+    // this.lazyTargets = document.querySelectorAll(".lazy-loading");
+    // console.log(this.lazyTargets);
+    // this.lazyTargets.forEach(lazyTarget => this.lazyLoad(lazyTarget));
+  }
+
+  lazyLoad(target: Element) {
+    const obs = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const img = entry.target;
+          const src = img.getAttribute("data-src");
+          img.setAttribute("src", src);
+          img.classList.add("fadeIn");
+          observer.disconnect(); //entry.target
+        }
+      });
+    });
+    obs.observe(target);
+  }
+}
